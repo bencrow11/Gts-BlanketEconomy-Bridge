d50c86d2ec93e68f4fb11c243ce9bd64a7e4aeff
Ben <bencrow11@gmail.com>
2025-01-02 18:57:56 +0000

    Initial Commit

---
+++ src/main/java/com/bencrow11/gtsblanketeconomybridge/GtsBlanketEconomyBridge.java
@@ -0,0 +1,10 @@
+package com.bencrow11.gtsblanketeconomybridge;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GtsBlanketEconomyBridge implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
